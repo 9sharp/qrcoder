e488e284be6c81a7fc17e27e093b3ae86a5f491b
Chenguang Zhang <chenguang.zhang@hulu.com>
2014-09-24 12:02:21 +0800

    Clear canvas before redrawing

--- examples/browser/public/assets/js/test_qrcoder.js
+++ examples/browser/public/assets/js/test_qrcoder.js
@@ -7,12 +7,17 @@ var cmpImage = new Image();
 var qrImage = new Image();
 var tarImage = new Image();
 
+function clearCanvas(obj) {
+    obj.getContext('2d').clearRect (0, 0, obj.width, obj.height);
+}
+
 cmpImage.onload = function() {
     qrImage.src = qrcoder.encodeQR(cmpImage.src);
 };
 
 tarImage.onload = function() {
-    tarCanvas.getContext('2d').drawImage(tarImage, 0, 0, 512, 512);
+    clearCanvas(tarCanvas);
+    tarCanvas.getContext('2d').drawImage(tarImage, 0, 0, tarCanvas.width, tarCanvas.height);
 };
 
 srcImage.onload = function() {
